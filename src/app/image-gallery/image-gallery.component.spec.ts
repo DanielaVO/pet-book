45209c90858c549e33c3fd7fdf213e442954f894
DanielaVOC <51915342+DanielaVOC@users.noreply.github.com>
2020-08-30 18:34:18 -0500

    Add test for image gallery

--- src/app/image-gallery/image-gallery.component.spec.ts
+++ src/app/image-gallery/image-gallery.component.spec.ts
@@ -1,16 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { GalleryComponent } from './image-gallery.component';
+import { GalleryComponent } from "./image-gallery.component";
+import { ImageService } from "../image.service";
+import { FilterimagesPipe } from "../filterimages.pipe";
 
-describe('ImageGalleryComponent', () => {
+describe("ImageGalleryComponent", () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GalleryComponent ]
-    })
-    .compileComponents();
+      declarations: [GalleryComponent, FilterimagesPipe],
+      providers: [
+        {
+          provide: ImageService,
+          useValue: {
+            getImages: () => {
+              return [
+                {
+                  id: 1,
+                  brand: "perro",
+                  url: "assets/images/perro1.jpg",
+                },
+              ];
+            },
+          },
+        },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +36,32 @@ describe('ImageGalleryComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("Debe crear el componente", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("Cuando se cargue el componente el tamaño de la lista debe ser uno", () => {
+    expect(component.allImages.length).toEqual(1);
+  });
+
+  it("Cuando se cargue el componente la lista debe contener el perro con id 1", () => {
+    component.allImages.forEach((item) => {
+      expect(item.id).toEqual(1);
+      expect(item.brand).toEqual("perro");
+      expect(item.url).toEqual("assets/images/perro1.jpg");
+    });
+  });
+
+  it("Cuando se cargue el componente debe tener 3 botones con los valores All, Perro y Gato", () => {
+    const buttons = fixture.nativeElement.querySelector(
+      "button"
+    );
+    const innerText = ["All", "Perro", "Gato"];
+    
+    expect(buttons.length).toEqual(3);
+    buttons.forEach((button) => {
+      const index = innerText.findIndex(button.innerText);
+      expect(index).toBeGreaterThanOrEqual(0);
+    });
   });
 });
