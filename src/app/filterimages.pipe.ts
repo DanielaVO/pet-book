0098c48d5b73523ee35a37df1359ebea81fd933a
Sebastian Luna <sebastian.x.luna.-ND@disney.com>
2020-10-23 20:26:41 -0500

    Fix bug

--- src/app/filterimages.pipe.ts
+++ src/app/filterimages.pipe.ts
@@ -8,7 +8,7 @@ export class FilterimagesPipe implements PipeTransform {
   transform(items: any[], laptop: string): any {    
     if(laptop === 'all'){ return items } else    
     return items.filter(item => {    
-      item.brand === laptop;    
+      return item.brand === laptop;    
     });
   }
 } 
